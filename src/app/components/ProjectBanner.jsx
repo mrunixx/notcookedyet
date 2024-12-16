c5ecfb2e20ca0722e22f1e44ceef40609befa36f
Manan Jaiswal <z5481212@ad.unsw.edu.au>
2024-12-16 15:47:26 +1100

    implemented localStorage for projects

--- src/app/components/ProjectBanner.jsx
+++ src/app/components/ProjectBanner.jsx
@@ -1,10 +1,37 @@
-const ProjectBanner = ({ name, onClick }) => {
+import { useState, useEffect } from "react";
+
+const ProjectBanner = ({ name, url, onClick }) => {
+  const [input, setInput] = useState(url);
+
+  useEffect(() => {
+    const projectsJSON = localStorage.getItem("projects");
+
+    if (projectsJSON) {
+      const projects = JSON.parse(projectsJSON);
+      const projectIndex = projects.findIndex((p) => p.name === name);
+      if (projectIndex !== -1) {
+        projects[projectIndex].url = input;
+        localStorage.setItem("projects", JSON.stringify(projects));
+      } 
+    }
+  }, [input]);
+
   return (
     <div className="course-banner flex">
       <div className="flex flex-col h-[5em] w-full bg-white text-black rounded-md flex py-2 px-5 justify-between">
         <p>{name}</p>
         <div className="flex justify-between items-center">
-          <button><img src="/link.png" alt="add url link for project" className="w-4"/></button>
+          <div className="link-input flex gap-2 w-full justify-center items-center">
+            <img src="/link.png" alt="add url link for project" className="w-4 h-4" />
+            <input
+              type="text"
+              name="url"
+              placeholder="url to github repository or deployment"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className="w-full"
+            />
+          </div>
           <button
             className="trash-btn w-10 rounded-md mild-glowing-button h-[2em] self-end"
             onClick={() => onClick(name)}
--- src/app/details/projects/page.jsx
+++ src/app/details/projects/page.jsx
@@ -1,63 +1,82 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TypeAnimationDetails from "../../components/TypeAnimationDetails";
 import ProjectBanner from "../../components/ProjectBanner";
+import Link from "next/link";
 
 const Projects = () => {
   const [projectName, setProjectName] = useState("");
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState(() => {
+    const storedData = localStorage.getItem("projects");
+    return storedData ? JSON.parse(storedData) : [];
+  })
 
   const handleProjectAdd = () => {
     const newProjects = [...projects];
-
-    setProjectName("")
-    if (newProjects.includes(projectName)) {
+    if (newProjects.length === 3) {
+      return;
+    }
+    if (newProjects.includes(projectName) || projectName === "") {
       return;
     }
 
-    newProjects.push(projectName);
-    setProjects(newProjects)
+    const newProject = {
+      name: projectName,
+      url: "",
+    };
+    setProjectName("");
+    newProjects.push(newProject);
+    setProjects(newProjects);
   };
 
   const handleProjectDelete = (value) => {
     const newProjects = [...projects];
-    const index = newProjects.indexOf(value);
+    const index = newProjects.indexOf((p) => JSON.parse(p).name === value);
     newProjects.splice(index, 1);
 
     setProjects(newProjects);
-  }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("projects", JSON.stringify(projects));
+  }, [projects]);
 
   return (
-    <div className="main-page-horizontal">
-      <div className="main-left">
-        <TypeAnimationDetails>Personal projects show initiative!</TypeAnimationDetails>
+    <div className="main-page w-1/2">
+      <TypeAnimationDetails>Personal projects show initiative!</TypeAnimationDetails>
+      <p>
+        Personal projects play a crucial role in a resume, especially when internships are not
+        available, as they demonstrate initiative and problem-solving skills, highlighting your
+        creativity, and passion, making you a strong candidate even without formal work experience.
+      </p>
+      <div className="details-input gap-2">
         <p>
-          Personal projects play a crucial role in a resume, especially when internships are not
-          available, as they demonstrate initiative and problem-solving skills, highlighting your
-          creativity, and passion, making you a strong candidate even without formal work
-          experience.
+          Projects
+          <span className="form-hint">Use 2-3 projects relating to your application.</span>
         </p>
-        <br />
-        <div className="details-input gap-2">
-          <p>
-            Projects
-            <span className="form-hint">Use 2-3 projects relating to your application.</span>
-          </p>
-          {projects.map((name, index) => {
-            return <ProjectBanner key={index} name={name} onClick={handleProjectDelete} />
-          })}
-          <div className="flex justify-center items-center w-full gap-2">
-            <input type="text" name="project-input" className="w-full" value={projectName} onChange={(e) => setProjectName(e.target.value)}/>
-            <button
-              className="w-10 h-[2em] flex justify-center items-center bg-white text-black rounded-md text-l mild-glowing-button"
-              onClick={handleProjectAdd}
-            >
-              &#43;
-            </button>
-          </div>
+        {projects.map((p, index) => {
+          return <ProjectBanner key={index} name={p.name} url={p.url} onClick={handleProjectDelete} />;
+        })}
+        <div className="flex justify-center items-center w-full gap-2">
+          <input
+            type="text"
+            name="project-input"
+            className="w-full"
+            value={projectName}
+            onChange={(e) => setProjectName(e.target.value)}
+            placeholder="e.g notcookedyet"
+          />
+          <button
+            className="w-10 h-[2em] flex justify-center items-center bg-white text-black rounded-md text-l mild-glowing-button"
+            onClick={handleProjectAdd}
+          >
+            &#43;
+          </button>
         </div>
       </div>
-      <div className="main-right"></div>
+      <Link href="/details/experience">
+        <button className="glowing-button" >Continue</button>
+      </Link>
     </div>
   );
 };
