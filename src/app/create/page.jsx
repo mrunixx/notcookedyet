a3fd5c6167564e3693ab0f7a71b819b71a956974
Manan Jaiswal <z5481212@ad.unsw.edu.au>
2024-12-18 02:54:52 +1100

    trying to implement using local storage to prefill the template

--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -6,21 +6,74 @@ import { useState } from "react";
 import SplitPane from "react-split-pane";
 
 const ResumeBuilderPage = () => {
-  const [value, setValue] = useState("");
+  const [personalDetails, setPersonalDetails] = useState(() => {
+    const data = JSON.parse(localStorage.getItem("personalDetails"));
+    return data ? data : {};
+  });
+  const [academicDetails, setAcademicDetails] = useState(() => {
+    const data = JSON.parse(localStorage.getItem("academicDetails"));
+    return data ? data : {};
+  });
+  const [projects, setProjects] = useState(() => {
+    const data = JSON.parse(localStorage.getItem("projects"));
+    return data ? data : [];
+  });
+  const [experiences, setExperiences] = useState(() => {
+    const data = JSON.parse(localStorage.getItem("experiences"));
+    return data ? data : [];
+  });
 
+  const [value, setValue] = useState(`
+  <div style="text-align: center;">
+
+  # ${personalDetails.firstName + " "} ${personalDetails.surname}
+
+  </div> 
+  <div class="headerInfo" style="
+    display: flex; 
+    justify-content: center;
+    align-items: center;
+    gap: 1em; 
+    text-decoration: none; 
+    color: #000000;"
+  >
+  ${personalDetails.email}
+  |
+  ${personalDetails.number}
+  |
+  ${personalDetails.github}
+  </div>
+
+  ## EDUCATION 
+  <strong>University of New South Wales</strong>
+
+  B. Computer Science, WAM: ${academicDetails.grade}
+
+  ## PROJECTS
+
+  ## EXPERIENCE 
+
+  ## TECHINCAL SKILLS 
+
+  - <strong>Languages</strong>: Please input application relevant languages you know here
+  - <strong>Technologies</strong>: Please input application relevant technologies here
+
+  ## EXTRACURRICULARS
+
+`);
   return (
     <div className="main-page" style={{ height: "100vh", display: "flex" }}>
       <SplitPane split="vertical" defaultSize="50%" minSize={300} maxSize={-300}>
         <div className="w-full editor p-[2em]" style={{ height: "100%" }}>
-          <MarkdownEditor 
-            source={value}
+          <MarkdownEditor
+            value={value}
             height="90vh"
             showToolbar={false}
             onChange={(val) => setValue(val)}
             enablePreview={false}
           />
         </div>
-        <div className="w-full preview" style={{ height: "100%", padding: "2em 6em"}}>
+        <div className="w-full preview" style={{ height: "100%", padding: "2em 6em" }}>
           <MarkdownPreview
             source={value}
             style={{
@@ -29,7 +82,8 @@ const ResumeBuilderPage = () => {
               padding: "2em",
               overflow: "auto",
               fontSize: "14px",
-              height: "100%"
+              height: "100%",
+              fontFamily: "Times New Roman"
             }}
           />
         </div>
