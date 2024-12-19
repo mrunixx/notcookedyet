f494a57b3bff320cf9f0051fa60bc95c5f5e7f2e
Manan Jaiswal <z5481212@ad.unsw.edu.au>
2024-12-19 12:02:35 +1100

    completed basic projects implementation:

--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -8,8 +8,23 @@ import SplitPane from "react-split-pane";
 const ResumeBuilderPage = () => {
   const personalDetails = JSON.parse(localStorage.getItem("personalDetails")) || {};
   const academicDetails = JSON.parse(localStorage.getItem("academicDetails")) || {};
-  // const projects = JSON.parse(localStorage.getItem("projects")) || [];
+  const projects = JSON.parse(localStorage.getItem("projects")) || [];
   // const experiences = JSON.parse(localStorage.getItem("experiences")) || [];
+
+  const createProjectsMDText = () => {
+    const mdStrings = [];
+    for (const project of projects) {
+      const mdString = `
+### ${project.name} | Place relevant technologies here  <span class="project-url">${project.url}</span>
+
+- Briefly describe your role in the project (e.g., Developer, Team Lead, Contributor).
+- List the main technologies, tools, or frameworks you used (e.g., React, Node.js, Firebase, etc.).
+- Quantify the results of your work 
+`
+      mdStrings.push(mdString);
+    }
+    return mdStrings.join('\n');
+  }
 
   const [value, setValue] = useState(`
 # ${personalDetails.firstName + " "} ${personalDetails.surname}
@@ -27,32 +42,34 @@ ${personalDetails.github}
 </div>
 
 ## EDUCATION 
-<strong>${academicDetails.universityName}</strong>
+**${academicDetails.universityName}**
 
 B. Computer Science, WAM: ${academicDetails.grade}
 
 ## PROJECTS
 
+${createProjectsMDText()}
+
 ## EXPERIENCE & EXTRACURRICULARS 
 
 ## TECHINCAL SKILLS 
 
-<strong>Languages</strong>: Please input application relevant languages you know here
+**Languages**: Please input application relevant languages you know here
 
-<strong>Technologies</strong>: Please input application relevant technologies here
-
-`);
+**Technologies**: Please input application relevant technologies here
+  `);
+  
   return (
     <div className="main-page" style={{ height: "100vh", display: "flex" }}>
       <SplitPane split="vertical" defaultSize="50%" minSize={300} maxSize={-300} allowResize={false}>
         <div className="w-full h-full editor" style={{ height: "100%" }}>
           <MarkdownEditor
             value={value}
-            height="90vh"
+            height="100vh"
             showToolbar={false}
             onChange={(val) => setValue(val)}
             enablePreview={false}
-            style={{ height: "100vh" }}
+            className="scrollable"
           />
         </div>
         <div className="w-full scrollable" style={{ padding: "2em 6em", overflowY: "auto", height: "100%" }}>
